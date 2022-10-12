7dc4f47307997e4560a6b1596f78de8d9505722c
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2022-10-12 16:58:45 +0530

    Routes done

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Market from "../pages/Market";
+import Create from "../pages/Create";
+import Contact from "../pages/Contact";
+import SellerProfile from "../pages/SellerProfile";
+import EditProfile from "../pages/EditProfile";
+import Wallet from "../pages/Wallet";
+import NftDetails from "../pages/NftDetails";
 
 const Routers = () => {
-  return <div>Routers</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/Home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/market" element={<Market />} />
+      <Route path="/create" element={<Create />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/seller-profile" element={<SellerProfile />} />
+      <Route path="/edit-profile" element={<EditProfile />} />
+      <Route path="/wallet" element={<Wallet />} />
+      <Route path="/market/:id" element={<NftDetails />} />
+    </Routes>
+  );
 };
 
 export default Routers;

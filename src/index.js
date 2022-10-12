c83841ac487e8b18c877b50f0ae25d6808f69c7b
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2022-10-12 15:47:21 +0530

    New pages added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
---
+++ src/routes/Routers.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Routers = () => {
+  return <div>Routers</div>;
+};
+
+export default Routers;

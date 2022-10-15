fe9f09e8335315a6507751fd3a0991196a8e5e9a
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2022-10-15 17:19:44 +0530

    Navbar sketch done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,69 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { Container } from "reactstrap";
+import "./Header.css";
+
+const NAV__LINKS = [
+  {
+    display: "Home",
+    url: "./home",
+  },
+  {
+    display: "Market",
+    url: "./market",
+  },
+  {
+    display: "Create",
+    url: "./create",
+  },
+  {
+    display: "Contact",
+    url: "./contact",
+  },
+];
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <Container>
+        <div className="navigation">
+          <div className="logo">
+            <h2 className=" d-flex gap-2 align-items-center ">
+              <span>
+                <i class="ri-fire-fill"></i>
+              </span>
+              NFTs
+            </h2>
+          </div>
+
+          <div className="nav__menu">
+            <ul className="nav__list">
+              {NAV__LINKS.map((item, index) => (
+                <li className="nav__item" key={index}>
+                  <NavLink to={item.url}>{item.display}</NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="nav__right">
+            <button className="btn">
+              <Link to="/wallet" className=" d-flex gap-2 align-items-center ">
+                <span>
+                  <i class="ri-wallet-line"></i>
+                </span>
+              </Link>
+              Connect Wallet
+            </button>
+
+            <span className="mobile__menu">
+              <i class="ri-menu-line"></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "remixicon/fonts/remixicon.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

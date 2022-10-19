b03fd9f01d15e62b9dd26aac3b9ac2b2e776c5f8
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2022-10-19 10:48:21 +0530

    HeroSection half done

---
+++ src/components/UI/HeroSection.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import "./HeroSection.css";
+
+const HeroSection = () => {
+  return (
+    <section className="hero__section">
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="hero__content">
+              <h2>
+                Discover rare digital art and collect
+                <span> sell extraorinary</span> NFTs
+              </h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
+                perspiciatis. Labore reprehenderit quia quae quibusdam
+                aspernatur excepturi natus, nostrum eligendi praesentium unde
+                debitis. Dolorem temporibus consequuntur ea blanditiis magnam
+                illo?
+              </p>
+              <div className="hero__btns d-flex align-items-center gap-4">
+                <button className=" explore__btn d-flex align-items-center gap-2 ">
+                  <i class="ri-rocket-line"></i>
+                  <Link to="/market">Explore</Link>
+                </button>
+                <button className=" create__btn d-flex align-items-center gap-2 ">
+                  <i class="ri-ball-pen-line"></i>
+                  <Link to="/create">Explore</Link>
+                </button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default HeroSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import HeroSection from "../components/UI/HeroSection";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
 };
 
 export default Home;

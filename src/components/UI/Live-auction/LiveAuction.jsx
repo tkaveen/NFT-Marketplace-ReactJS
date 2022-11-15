7023994114cc5e7479ab5eefb0c4117d37541c79
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2022-11-15 15:43:00 +0530

    NFT Card mapping done

--- src/components/UI/Live-auction/LiveAuction.jsx
+++ src/components/UI/Live-auction/LiveAuction.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
-import img1 from "../../../assets/images/img-01.jpg";
-import avt1 from "../../../assets/images/ava-01.png";
 import "./LiveAuction.css";
+import NftCard from "../Nft-card/NftCard";
+import { NFT__DATA } from "../../../assets/data/data";
 
 const LiveAuction = () => {
   return (
     <section>
       <Container>
         <Row>
-          <Col lg="12" className="mb-4">
+          <Col lg="12" className="mb-5">
             <div className="live__auction__top d-flex align-items-center justify-content-between">
               <h3>Live Auction</h3>
               <span>
@@ -20,41 +20,9 @@ const LiveAuction = () => {
           </Col>
 
           <Col lg="3">
-            <div className="single__nft__card">
-              <div className="nft__img">
-                <img src={img1} alt="" className="w-100" />
-              </div>
-
-              <div className="nft__content">
-                <h5 className="nft__title">Travel Monkey Club</h5>
-
-                <div className="creator__info_wrapper d-flex gap-3">
-                  <div className="creator__img">
-                    <img src={avt1} alt="" className="w-100" />
-                  </div>
-
-                  <div className="creator__info w-100 d-flex align-items-center justify-content-between">
-                    <div>
-                      <h6>Created By</h6>
-                      <p>Trista Francis</p>
-                    </div>
-                    <div>
-                      <h6>Current Bid</h6>
-                      <p>4.89 ETH</p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className=" mt-3 d-flex align-items-center justify-content-between">
-                  <button className="bid__btn d-flex align-items-center gap-1">
-                    <i class="ri-shopping-bag-line"></i> Place Bid
-                  </button>
-                  <span className='history__link'>
-                    <Link to="#">View History</Link>
-                  </span>
-                </div>
-              </div>
-            </div>
+            {NFT__DATA.slice(0, 4).map((item) => (
+              <NftCard key={item.id} item={item} />
+            ))}
           </Col>
         </Row>
       </Container>
---
+++ src/components/UI/Nft-card/NftCard.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NftCard.css";
+
+const NftCard = (props) => {
+  const { title, id, currentBid, creatorImg, imgUrl, creator } = props.item;
+
+  return (
+    <div className="single__nft__card">
+      <div className="nft__img">
+        <img src={imgUrl} alt="" className="w-100" />
+      </div>
+
+      <div className="nft__content">
+        <h5 className="nft__title">
+          <Link to={`/market/${id}`}>{title}</Link>
+        </h5>
+
+        <div className="creator__info_wrapper d-flex gap-3">
+          <div className="creator__img">
+            <img src={creatorImg} alt="" className="w-100" />
+          </div>
+
+          <div className="creator__info w-100 d-flex align-items-center justify-content-between">
+            <div>
+              <h6>Created By</h6>
+              <p>{creator}</p>
+            </div>
+            <div>
+              <h6>Current Bid</h6>
+              <p>{currentBid}</p>
+            </div>
+          </div>
+        </div>
+
+        <div className=" mt-3 d-flex align-items-center justify-content-between">
+          <button className="bid__btn d-flex align-items-center gap-1">
+            <i class="ri-shopping-bag-line"></i> Place Bid
+          </button>
+          <span className="history__link">
+            <Link to="#">View History</Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NftCard;
